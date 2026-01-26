95b7dbae97cc9444b4d232e269392fc05cd3e96f
Gabrielly Lorraynne <gabyalmeidacom@hotmail.com>
2026-01-26 19:42:22 -0400

    Listar vagas com filtro opcional

--- src/main/java/com/api/parking_control/application/port/out/ParkingSpotPort.java
+++ src/main/java/com/api/parking_control/application/port/out/ParkingSpotPort.java
@@ -2,6 +2,11 @@ package com.api.parking_control.application.port.out;
 
 import com.api.parking_control.domain.model.ParkingSpot;
 
+import java.util.List;
+
 public interface ParkingSpotPort {
     ParkingSpot salvar(ParkingSpot parkingSpot);
+    List<ParkingSpot> listarVagas();
+
+    List<ParkingSpot> listarVagasPorApartamento(String apartment);
 }
--- src/main/java/com/api/parking_control/application/service/ParkingSpotService.java
+++ src/main/java/com/api/parking_control/application/service/ParkingSpotService.java
@@ -1,21 +1,33 @@
 package com.api.parking_control.application.service;
 
 import com.api.parking_control.application.port.in.CreateParkingSpotUseCasePort;
+import com.api.parking_control.application.port.in.ListParkingSpotUseCasePort;
 import com.api.parking_control.application.port.out.ParkingSpotPort;
 import com.api.parking_control.domain.model.ParkingSpot;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ParkingSpotService implements CreateParkingSpotUseCasePort {
+public class ParkingSpotService implements CreateParkingSpotUseCasePort, ListParkingSpotUseCasePort {
     private final ParkingSpotPort port;
 
     @Override
     public ParkingSpot cadastrarVaga(ParkingSpot parkingSpot) {
         parkingSpot.setRegistrationDate(LocalDateTime.now());
         return port.salvar(parkingSpot);
+    }
+
+    @Override
+    public List<ParkingSpot> listarVagas(String apartment) {
+
+        if(apartment == null || apartment.isBlank()) {
+            return port.listarVagas();
+        }
+
+        return port.listarVagasPorApartamento(apartment);
     }
 }
